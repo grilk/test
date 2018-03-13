3a894feea51fd4d9bb770d08118ebf21b7b2512b
grilk <virvelvind@hotmail.com>
2018-03-13 16:06:43 +0100

    test
    test

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -33,6 +33,11 @@ export class NavMenu extends Component {
                 <Glyphicon glyph='th-list' /> Fetch data
               </NavItem>
             </LinkContainer>
+            <LinkContainer to={'/test'}>
+                <NavItem>
+                    <Glyphicon glyph='th-list' /> Test
+              </NavItem>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
