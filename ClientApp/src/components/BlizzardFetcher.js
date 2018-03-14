4ebd758726a6e9522a5be5585588513e585a642c
grilk <virvelvind@hotmail.com>
2018-03-14 15:15:27 +0100

    realms
    changed bosses to realmstatus

--- ClientApp/src/components/BlizzardFetcher.js
+++ ClientApp/src/components/BlizzardFetcher.js
@@ -1,5 +1,5 @@
 ﻿import React, { Component } from 'react';
-const url = 'https://us.api.battle.net/wow/boss/?locale=en_US&apikey=4vb48bnnfazwve7gsy7ttgukavur86hf'
+const url = 'https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=4vb48bnnfazwve7gsy7ttgukavur86hf'
 
 export class BlizzardFetcher extends Component {
     constructor() {
@@ -12,9 +12,9 @@ export class BlizzardFetcher extends Component {
         fetch(url)
             .then((Response) => Response.json())
             .then((findresponse) => {
-                console.log(findresponse.bosses);
+                console.log(findresponse.realms);
                 this.setState({
-                    data: findresponse.bosses
+                    data: findresponse.realms
                 })
             })
     }
@@ -22,23 +22,28 @@ export class BlizzardFetcher extends Component {
         return (
             <div>
                 {
-                    //this.state.data.map((bosses) =>
-                    //        <li key={bosses.id}>
-                    //    {bosses.name}
-                    //    </li>
-
                     <table className='table'>
                         <thead>
                             <tr>
                                 <th>Name</th>
-                                <th>Description</th>
+                                <th>Type</th>
+                                <th>Population</th>
+                                <th>Queue</th>
+                                <th>Status</th>
+                                <th>Battlegroup</th>
+                                <th>Timezone</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {this.state.data.map(bosses =>
-                                <tr key={bosses.id}>
-                                    <td>{bosses.name}</td>
-                                    <td>{bosses.description}</td>
+                            {this.state.data.map(realm =>
+                                <tr key={realm.slug}>
+                                    <td>{realm.name}</td>
+                                    <td>{realm.type}</td>
+                                    <td>{realm.population}</td>
+                                    <td>{realm.queue}</td>
+                                    <td>{realm.status}</td>
+                                    <td>{realm.battlegroup}</td>
+                                    <td>{realm.timezone}</td>
                                 </tr>
                             )}
                         </tbody>
